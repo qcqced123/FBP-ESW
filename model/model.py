03f619717a87202f6b10549bc157610ea88645ec
Lee <97833081+qcqced123@users.noreply.github.com>
2023-06-16 15:30:51 +0900

    Add Sequence Dataset debugging code

--- model/model.py
+++ model/model.py
@@ -29,48 +29,6 @@ class DeBERTaModel(nn.Module):
             config=self.auto_cfg
         )
         self.fc = nn.Linear(self.auto_cfg.hidden_size, 15)  # BIO Style NER Task
-        if self.cfg.load_pretrained:
-            self.model.load_state_dict(
-                torch.load(cfg.checkpoint_dir + cfg.state_dict),
-                strict=False
-            )  # load student model's weight: it already has fc layer, so need to init fc layer later
-
-        if cfg.reinit:
-            self._init_weights(self.fc)
-            reinit_topk(self.model, cfg.num_reinit)
-
-        if cfg.freeze:
-            freeze(self.model.embeddings)
-            freeze(self.model.encoder.layer[:cfg.num_freeze])
-
-        if cfg.gradient_checkpoint:
-            self.model.gradient_checkpointing_enable()
-
-    def _init_weights(self, module) -> None:
-        """ over-ride initializes weights of the given module function (+initializes LayerNorm) """
-        if isinstance(module, nn.Linear):
-            if self.cfg.init_weight == 'normal':
-                module.weight.data.normal_(mean=0.0, std=self.auto_cfg.initializer_range)
-            elif self.cfg.init_weight == 'xavier_uniform':
-                module.weight.data = nn.init.xavier_uniform_(module.weight.data)
-            elif self.cfg.init_weight == 'xavier_normal':
-                module.weight.data = nn.init.xavier_normal_(module.weight.data)
-            elif self.cfg.init_weight == 'kaiming_uniform':
-                module.weight.data = nn.init.kaiming_uniform_(module.weight.data)
-            elif self.cfg.init_weight == 'kaiming_normal':
-                module.weight.data = nn.init.kaiming_normal_(module.weight.data)
-            elif self.cfg.init_weight == 'orthogonal':
-                module.weight.data = nn.init.orthogonal_(module.weight.data)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.auto_cfg.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            """ reference from torch.nn.Layernorm with elementwise_affine=True """
-            module.weight.data.fill_(1.0)
-            module.bias.data.zero_()
 
     def feature(self, inputs_ids, attention_mask, token_type_ids):
         outputs = self.model(
